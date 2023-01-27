8d8950cdc78e9426bb0f58e3ef7cdcedd80db38c
inerer <arshinov04@gmail.com>
2023-01-27 13:57:13 +0300

    finally repos

--- Cookbook/Database/Repositories/Interfaces/RecipeInterfaces/IRecipeStatsRepository.cs
+++ Cookbook/Database/Repositories/Interfaces/RecipeInterfaces/IRecipeStatsRepository.cs
@@ -8,8 +8,6 @@ namespace Cookbook.Database.Repositories.Interfaces.RecipeInterfaces;
 public interface IRecipeStatsRepository
 {
     public Task<RecipeStats> GetRecipeStatsAsync(int id);
-    public Task<RecipeStats> GetRecipeStatsByRecipeAsync(int recipeId);
-    public Task<List<RecipeStats>> GetRecipeStatsAsync();
     public Task<CommandResult> AddRecipeStatsAsync(RecipeStats recipeStats);
     public Task<CommandResult> UpdateRecipeStatsAsync(RecipeStats recipeStats);
     public Task<CommandResult> DeleteRecipeStatsAsync(int id);
--- Cookbook/Database/Repositories/Recipe/RecipeStatsRepository.cs
+++ Cookbook/Database/Repositories/Recipe/RecipeStatsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 using Cookbook.Database.Repositories.Interfaces.RecipeInterfaces;
 using Cookbook.Models.Database;
@@ -12,27 +13,104 @@ public class RecipeStatsRepository : MainDbClass, IRecipeStatsRepository
 {
     public async Task<RecipeStats> GetRecipeStatsAsync(int id)
     {
-        throw new System.NotImplementedException();
-    }
-
-    public async Task<RecipeStats> GetRecipeStatsByRecipeAsync(int recipeId)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public async Task<List<RecipeStats>> GetRecipeStatsAsync()
-    {
-        throw new System.NotImplementedException();
+        connection.Open();
+        try
+        {
+            RecipeStats recipeStats = new RecipeStats();
+            string query = $"select * from recipe_stats where recipe_id = $1";
+            await using NpgsqlCommand cmd = new NpgsqlCommand(query, connection)
+            {
+                Parameters = { new() { Value = id} }
+            };
+            await using NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+            
+            while(await reader.ReadAsync())
+            {
+                recipeStats.RecipeId = reader.GetInt32(reader.GetOrdinal("recipe_id"));
+                recipeStats.Squirrels = reader.GetDecimal(reader.GetOrdinal("squirrls"));
+                recipeStats.Fats = reader.GetDecimal(reader.GetOrdinal("fats"));
+                recipeStats.Carbohydrates = reader.GetDecimal(reader.GetOrdinal("carbohydrates"));
+                recipeStats.Kilocalories = reader.GetDecimal(reader.GetOrdinal("kilocalories"));
+            }
+            
+            return recipeStats;
+        }
+        catch
+        {
+            return null;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
     }
 
     public async Task<CommandResult> AddRecipeStatsAsync(RecipeStats recipeStats)
     {
-        throw new System.NotImplementedException();
+        CommandResult result;
+        connection.Open();
+        try
+        {
+            string query = $"insert into recipe_stats(recipe_id, squirrels, fats, carbohydrates, kilocalories) values ($1, $2, $3, $4, $5) returning id";
+            await using NpgsqlCommand cmd = new NpgsqlCommand(query, connection)
+            {
+                Parameters =
+                {
+                    new() { Value = recipeStats.RecipeId },
+                    new() { Value = recipeStats.Squirrels },
+                    new() { Value = recipeStats.Fats },
+                    new() { Value = recipeStats.Carbohydrates },
+                    new() { Value = recipeStats.Kilocalories }
+                }
+            }; 
+            result = CommandResults.Successfully;
+            result.ValueId = await cmd.ExecuteNonQueryAsync();
+            return result;
+        }
+        catch(Exception e)
+        {
+            result = CommandResults.BadRequest;
+            result.Description = e.ToString();
+            return result;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
     }
 
     public async Task<CommandResult> UpdateRecipeStatsAsync(RecipeStats recipeStats)
     {
-        throw new System.NotImplementedException();
+        CommandResult result;
+        connection.Open();
+        try
+        {
+            string query = $"update recipe_stats set squirrels = $2, fats = $3, carbohydrates = $4, kilocalories = $5 where recipe_id = $1";
+            await using NpgsqlCommand cmd = new NpgsqlCommand(query, connection)
+            {
+                Parameters =
+                {
+                    new() { Value = recipeStats.RecipeId },
+                    new() { Value = recipeStats.Squirrels },
+                    new() { Value = recipeStats.Fats },
+                    new() { Value = recipeStats.Carbohydrates },
+                    new() { Value = recipeStats.Kilocalories }
+                }
+            }; 
+            result = CommandResults.Successfully;
+            result.ValueId = await cmd.ExecuteNonQueryAsync();
+            return result;
+        }
+        catch(Exception e)
+        {
+            result = CommandResults.BadRequest;
+            result.Description = e.ToString();
+            return result;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
     }
 
     public async Task<CommandResult> DeleteRecipeStatsAsync(int id)
