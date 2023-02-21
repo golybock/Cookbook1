0ad5196a2afed07bcf36083fd1e44464b60bd5c9
golybock <teplovartem3094@gmail.com>
2023-02-21 21:32:26 +0300

    aboba

---
+++ Cookbook/Pages/Recipe/EditRecipePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Cookbook.Pages.Recipe;
+
+public partial class EditRecipePage : Page
+{
+    public EditRecipePage()
+    {
+        InitializeComponent();
+    }
+}
